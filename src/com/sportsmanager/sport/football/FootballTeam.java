666e9448e71b11a292c4d3f22d9276a7422a8096
parcalarCuma <cumaparcalar334@gmail.com>
2026-03-14 20:56:16 +0300

    Create FootballTeam.java
    I implemented  FootballTeam  concrate  class.

---
+++ src/com/sportsmanager/sport/football/FootballTeam.java
@@ -0,0 +1,7 @@
+package com.sportsmanager.sport.football;
+
+import com.sportsmanager.core.AbstractTeam;
+import com.sportsmanager.core.IPlayer;
+
+public class FootballTeam extends AbstractTeam {
+}
