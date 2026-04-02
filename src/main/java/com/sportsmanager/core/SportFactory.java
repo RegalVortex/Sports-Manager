20c0e9d59769b390fd0ae91ae49239a4afc2439e
RegalVortex <kagansevgi@gmail.com>
2026-04-03 00:14:02 +0300

    SportFactory Updated contents
    required functions added

--- src/main/java/com/sportsmanager/core/SportFactory.java
+++ src/main/java/com/sportsmanager/core/SportFactory.java
@@ -1,5 +1,22 @@
 package com.sportsmanager.core;
 
+import java.util.List;
+
 public interface SportFactory {
-  
+
+    ISport createSport();
+
+    IPlayer createPlayer(String name, String position);
+
+    ITeam createTeam(String name, String logoPath);
+
+    IMatch createMatch(ITeam home, ITeam away, int week);
+
+    ILeague createLeague(String name, List<ITeam> teams);
+
+    ICoach createCoach(String name, String specialty);
+
+    ITactic createDefaultTactic();
+
+    List<ITactic> getAvailableTactics();
 }
