60d7f866853cfb034d589bacec0adf529f11bf55
parcalarCuma <cumaparcalar334@gmail.com>
2026-04-03 00:24:02 +0300

    Prepared  IPlayer İnterface
    I prepared IPlayer  interface.

--- src/main/java/com/sportsmanager/core/IPlayer.java
+++ src/main/java/com/sportsmanager/core/IPlayer.java
@@ -1,4 +1,23 @@
 package com.sportsmanager.core;
 
+import java.util.Map;
+
 public interface IPlayer {
+    String getName();
+
+    String getPosition();
+
+    Map<String, Integer> getAttributes();
+
+    int getOverallRating();
+
+    boolean isInjured();
+
+    int getInjuryGamesRemaining();
+
+    void setInjured(int games);
+
+    void decrementInjury();
+
+    void train(String attribute, int amount);
 }
