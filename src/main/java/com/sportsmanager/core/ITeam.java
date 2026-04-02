99f17152f1eaf835722ed07bf5ceb0d1a95cb442
IlkerGedik <ilkergedik2@gmail.com>
2026-04-02 22:34:08 +0300

    Add methods for player management and scoring

--- src/main/java/com/sportsmanager/core/ITeam.java
+++ src/main/java/com/sportsmanager/core/ITeam.java
@@ -1,5 +1,25 @@
 package com.sportsmanager.core;
 
+import java.util.List;
+
 public interface ITeam {
+
+       String getName();
+
+    List<IPlayer> getSquad();
+
+    List<IPlayer> getStartingLineup();
+
+    ICoach getCoach();
+
+    ITactic getTactic();
+
+    void setTactic(ITactic tactic);
+
+    void substitutePlayer(IPlayer out, IPlayer in);
+
+    void addPoints(int points);
+
+    int getPoints();
    
 }
