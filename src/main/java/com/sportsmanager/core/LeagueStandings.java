5eda3e47299f7275e87c7dfb4b9bbc2ab747ea84
parcalarCuma <cumaparcalar334@gmail.com>
2026-04-02 19:51:03 +0300

    Prepared to the LeagueStandings
    I prepared the LeagueStandings .

--- src/main/java/com/sportsmanager/core/LeagueStandings.java
+++ src/main/java/com/sportsmanager/core/LeagueStandings.java
@@ -1,4 +1,24 @@
 package com.sportsmanager.core;
 
+import java.util.List;
+
 public class LeagueStandings {
+
+    private final List<ITeam> rankedTeams;
+
+    public LeagueStandings(List<ITeam> rankedTeams) {
+        this.rankedTeams = rankedTeams;
+
+    }
+
+
+    public List<ITeam> getTeams(){
+        return  rankedTeams;
+    }
+
+
+    public  int getRankOf(ITeam team){
+        return rankedTeams.indexOf(team)+1;
+
+    }
 }
