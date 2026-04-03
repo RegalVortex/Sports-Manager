cddd133a4f8b6232ab3988c2551c66ccdd6f3987
parcalarCuma <cumaparcalar334@gmail.com>
2026-04-03 17:24:00 +0300

    prepared  ICoach
     I prepared  ICoach   interface

--- src/main/java/com/sportsmanager/core/ICoach.java
+++ src/main/java/com/sportsmanager/core/ICoach.java
@@ -1,4 +1,11 @@
 package com.sportsmanager.core;
 
+import java.util.List;
+
 public interface ICoach {
+
+    String getName();
+    String getSpecialty();
+    void  trainPlayers(List<IPlayer> players);
+
 }
