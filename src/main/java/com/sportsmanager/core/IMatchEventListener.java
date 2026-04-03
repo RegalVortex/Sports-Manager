f1defc64dcc05e98d2d5a901f6e889d1fe486de9
RegalVortex <kagansevgi@gmail.com>
2026-04-03 12:49:03 +0300

    implementation of ISport and IMatchEventListener interfaces

--- src/main/java/com/sportsmanager/core/IMatchEventListener.java
+++ src/main/java/com/sportsmanager/core/IMatchEventListener.java
@@ -1,4 +1,8 @@
 package com.sportsmanager.core;
 
 public interface IMatchEventListener {
+
+    void onMatchEvent(String event);
+
+    void onMatchComplete(MatchResult result);
 }
--- src/main/java/com/sportsmanager/core/ISport.java
+++ src/main/java/com/sportsmanager/core/ISport.java
@@ -1,4 +1,20 @@
 package com.sportsmanager.core;
+
+import java.util.List;
+
 public interface ISport {
-  
+
+    String getSportName();
+
+    int getTeamSize();
+
+    int getMaxSubstitutes();
+
+    int getNumberOfPeriods();
+
+    String getPeriodName();
+
+    List<String> getValidPositions();
+
+    SportFactory createFactory();
 }
