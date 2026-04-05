c9a898ce1ef222ce78a0d7033e34559c546938f2
parcalarCuma <cumaparcalar334@gmail.com>
2026-04-05 18:52:13 +0300

    Prepared MatchResult
    I prepared   MatchResult class.

--- src/main/java/com/sportsmanager/core/MatchResult.java
+++ src/main/java/com/sportsmanager/core/MatchResult.java
@@ -1,4 +1,43 @@
 package com.sportsmanager.core;
 
 public class MatchResult {
+
+    private final ITeam homeTeam;
+    private final ITeam awayTeam;
+    private final int homeScore;
+    private final int awayScore;
+
+    public MatchResult(ITeam homeTeam, ITeam awayTeam, int homeScore, int awayScore) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = homeScore;
+        this.awayScore = awayScore;
+    }
+
+    public ITeam getHomeTeam() {
+        return homeTeam;
+    }
+
+    public ITeam getAwayTeam() {
+        return awayTeam;
+    }
+
+    public int getHomeScore() {
+        return homeScore;
+    }
+
+    public int getAwayScore() {
+        return awayScore;
+    }
+
+    public ITeam getWinner() {
+        if (homeScore > awayScore) return homeTeam;
+        if (awayScore > homeScore) return awayTeam;
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return homeTeam.getName() + " " + homeScore + " - " + awayScore + " " + awayTeam.getName();
+    }
 }
