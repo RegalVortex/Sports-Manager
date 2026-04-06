edb0c33ac31113279f0a1c4c118764bca732c2b1
RegalVortex <kagansevgi@gmail.com>
2026-04-06 10:45:49 +0300

    FootballCoach.java completed

--- src/main/java/com/sportsmanager/sport/football/FootballCoach.java
+++ src/main/java/com/sportsmanager/sport/football/FootballCoach.java
@@ -1,7 +1,46 @@
 package com.sportsmanager.sport.football;
 
 import com.sportsmanager.core.ICoach;
+import com.sportsmanager.core.IPlayer;
 
-public class FootballCoach{
+import java.util.List;
 
+public class FootballCoach implements ICoach {
+
+    private String name;
+    private String specialty;
+
+    public FootballCoach(String name, String specialty) {
+        this.name = name;
+        this.specialty = specialty;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    @Override
+    public void trainPlayers(List<IPlayer> players) {
+        for (IPlayer player : players) {
+            if (player.isInjured()) {
+                continue;
+            }
+
+            if ("ATTACKING".equalsIgnoreCase(specialty)) {
+                player.train("shooting", 2);
+                player.train("pace", 1);
+            } else if ("DEFENSIVE".equalsIgnoreCase(specialty)) {
+                player.train("defending", 2);
+                player.train("heading", 1);
+            } else if ("FITNESS".equalsIgnoreCase(specialty)) {
+                player.train("stamina", 2);
+            }
+        }
+    }
 }
