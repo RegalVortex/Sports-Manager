6cde2a8641b2a21113be567800aeac21a602b597
parcalarCuma <cumaparcalar334@gmail.com>
2026-04-24 14:49:24 +0300

    Update AbstractLeague.java
    I Updated The AbstractLeague for new
    requirements.

--- src/main/java/com/sportsmanager/core/AbstractLeague.java
+++ src/main/java/com/sportsmanager/core/AbstractLeague.java
@@ -91,18 +91,68 @@ public abstract class AbstractLeague implements ILeague {
     protected void generateFixtures() {
         fixtures.clear();
 
+        List<ITeam> rotationTeams = new ArrayList<>(teams);
+
+        if (rotationTeams.size() % 2 != 0) {
+            rotationTeams.add(null);
+        }
+
+        int numberOfTeams = rotationTeams.size();
+        int rounds = numberOfTeams - 1;
+        int matchesPerWeek = numberOfTeams / 2;
+
         int week = 1;
 
-        for (int i = 0; i < teams.size(); i++) {
-            for (int j = i + 1; j < teams.size(); j++) {
-                ITeam home = teams.get(i);
-                ITeam away = teams.get(j);
 
-                fixtures.add(createMatch(home, away, week++));
-                fixtures.add(createMatch(away, home, week++));
+        for (int round = 0; round < rounds; round++) {
+            for (int matchIndex = 0; matchIndex < matchesPerWeek; matchIndex++) {
+                ITeam home = rotationTeams.get(matchIndex);
+                ITeam away = rotationTeams.get(numberOfTeams - 1 - matchIndex);
+
+                if (home != null && away != null) {
+                    fixtures.add(createMatch(home, away, week));
+                }
             }
+
+            rotateTeams(rotationTeams);
+            week++;
+        }
+
+
+        int firstHalfFixtureCount = fixtures.size();
+
+        for (int i = 0; i < firstHalfFixtureCount; i++) {
+            IMatch firstLeg = fixtures.get(i);
+
+            ITeam home = firstLeg.getAwayTeam();
+            ITeam away = firstLeg.getHomeTeam();
+
+            int reverseWeek = getWeekOfMatch(firstLeg) + rounds;
+
+            fixtures.add(createMatch(home, away, reverseWeek));
         }
     }
+
+    private void rotateTeams(List<ITeam> rotationTeams) {
+        if (rotationTeams.size() <= 2) {
+            return;
+        }
+
+        ITeam fixedTeam = rotationTeams.get(0);
+        ITeam lastTeam = rotationTeams.remove(rotationTeams.size() - 1);
+
+        rotationTeams.add(1, lastTeam);
+        rotationTeams.set(0, fixedTeam);
+    }
+
+    private int getWeekOfMatch(IMatch match) {
+        if (match instanceof AbstractMatch) {
+            AbstractMatch abstractMatch = (AbstractMatch) match;
+            return abstractMatch.getWeek();
+        }
+        return 0;
+    }
+
 
     protected int calculateTotalWeeks() {
         int maxWeek = 0;
